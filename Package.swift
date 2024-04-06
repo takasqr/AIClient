015012b6a63bb2f9a90f4006c374e4925304d6b0
takasqr <95556025+takasqr@users.noreply.github.com>
2024-04-06 14:19:47 +0900

    ChatGPTClient

--- Package.swift
+++ Package.swift
@@ -11,11 +11,17 @@ let package = Package(
             name: "AIClient",
             targets: ["AIClient"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/takasqr/SwiftWing", .upToNextMajor(from: "0.1.4")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AIClient"),
+            name: "AIClient",
+            dependencies: [
+                "SwiftWing"
+            ]),
         .testTarget(
             name: "AIClientTests",
             dependencies: ["AIClient"]),
